651cf19e45a8a7a5941a249c3b877a96fe7dbf16
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2020-05-19 01:25:23 +0200

    Initial upload

---
+++ block.js
@@ -0,0 +1,61 @@
+class Block {
+  constructor(x, size, mass, velocity, constraint) {
+    this.x = x;
+    this.y = height - size;
+    this.size = size;
+    this.mass = mass;
+    this.velocity = velocity;
+    this.counter = 0;
+    this.constraint = constraint;
+    this.collision = false;
+  }
+
+  move() {
+    this.x = this.x - this.velocity;
+    if (this.x <= 0 && !this.collision) {
+      this.x = 0;
+      this.counter = this.counter + 1;
+      this.velocity = -(this.velocity);
+      return true;
+      // clack.play();
+    }
+    return false;
+  }
+
+  checkForBounce(block) {
+    if (this.x <= block.x + block.size) {
+      newVelocity(block, this);
+      block.x = this.x - block.size - 1;
+      this.counter = this.counter + 1;
+      this.collision = true;
+      return true;
+      // clack.play();
+    } else {
+      this.collision = false;
+      return false;
+    }
+  }
+  
+
+  display() {
+    if (this.size === 50) {
+      image(panda, max(this.x, this.constraint), this.y, this.size, this.size);
+    } else {
+      image(bear, max(this.x, this.constraint), this.y, this.size, this.size);
+    }
+  }
+}
+
+const newVelocity = (blockSmall, blockBig) => {
+  let v1, v2;
+  const m1 = blockSmall.mass;
+  const m2 = blockBig.mass;
+  const u1 = blockSmall.velocity;
+  const u2 = blockBig.velocity;
+  
+  v1 = (((m1 - m2)/(m1 + m2)) * u1) + (((2 * m2)/(m1 + m2)) * u2);
+  v2 = (((2 * m1)/(m1 + m2)) * u1) + (((m2 - m1)/(m1 + m2)) * u2);
+  
+  blockSmall.velocity = v1;
+  blockBig.velocity = v2;
+}
---
+++ sketch.js
@@ -0,0 +1,78 @@
+///
+let DIGITS = 5;
+///
+
+let blockSmall;
+let blockBig;
+let countDiv;
+let counter = 0;
+let collision = false;
+let slider;
+
+const timeSkip = 1000000;
+
+function setup() {
+  createCanvas(windowWidth, 400);
+  blockSmall = new Block(100, 50, 1, 0, 0);
+  blockBig = new Block(max(windowWidth / 3, 250), 150, pow(100, DIGITS - 1), 5 / timeSkip, 50);
+  
+  countDiv = createElement('h2','Count: ' + counter);
+  countDiv.style('font-size', '52pt');
+  countDiv.style('color', 'white');
+  
+  piDiv = createElement('p', 'Pi: ' + PI.toFixedNoRounding(DIGITS - 1) + '...');
+  piDiv.style('font-size', '22pt');
+  piDiv.style('color', 'white');
+  digitsDiv = createElement('p', 'Digits to calculate: ' + DIGITS);
+  digitsDiv.style('font-size', '22pt');
+  digitsDiv.style('color', 'white');
+  slider = createSlider(1, 9, DIGITS);
+  let resetButton = createButton('Reset');
+  resetButton.mousePressed(reset);
+  slider.input(() => {
+    DIGITS = slider.value();
+    digitsDiv.html(nf('Digits to calculate: ' + DIGITS));
+    piDiv.html(nf('Pi: ' + PI.toFixedNoRounding(DIGITS - 1) + '...'));
+  });
+
+}
+
+function preload() {
+  clack = loadSound('clack.wav');
+  panda = loadImage('panda.png');
+  bear = loadImage('bear.png');
+}
+
+function draw() {
+  background(10);
+  collision = false;
+  for (let i = 0; i < timeSkip; i++) {
+    let wallBounce = blockSmall.move();
+    blockBig.move();
+    let collisionBounce = blockBig.checkForBounce(blockSmall);
+    if (collisionBounce || wallBounce) collision = true;
+  }
+  if (collision) clack.play();
+  counter = blockSmall.counter + blockBig.counter;
+  blockSmall.display();
+  blockBig.display();
+  countDiv.html(nf('Count: ' + counter));
+}
+
+function reset() {
+  DIGITS = slider.value();
+  blockSmall = new Block(100, 50, 1, 0, 0);
+  blockBig = new Block(max(windowWidth / 3, 250), 150, pow(100, DIGITS - 1), 5 / timeSkip, 50);
+  
+}
+
+Number.prototype.toFixedNoRounding = function(n) {
+    const reg = new RegExp("^-?\\d+(?:\\.\\d{0," + n + "})?", "g")
+    const a = this.toString().match(reg)[0];
+    const dot = a.indexOf(".");
+    if (dot === -1) {
+        return a + "." + "0".repeat(n);
+    }
+    const b = n - (a.length - dot) + 1;
+    return b > 0 ? (a + "0".repeat(b)) : a;
+ }
